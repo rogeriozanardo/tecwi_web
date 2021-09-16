24eacfd00f013a7a6d762516e3d8fd3eb03358bf
Gabriel H. Foli <gabriel.foli@outlook.com>
2021-09-15 22:55:54 -0300

    - Desenvolvido as telas inicias (Login, home e dashboards)

---
+++ TecWi_Web.WASM/Pages/Home/Home.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecWi_Web.WASM.Pages.Home
+{
+    partial class Home
+    {
+        public void GestaoCobranca_OnClick()
+        {
+            Navigation.NavigateTo("/GestaoCobranca");
+        }
+
+        public void GestaoLogistica_OnClick()
+        {
+            Navigation.NavigateTo("/GestaoLogistica");
+        }
+
+        public void Configuracoes_OnClick()
+        {
+            Navigation.NavigateTo("/Configuracoes");
+        }
+    }
+}
--- TecWi_Web.WASM/Program.cs
+++ TecWi_Web.WASM/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 
 namespace TecWi_Web.WASM
 {
@@ -14,11 +12,14 @@ namespace TecWi_Web.WASM
     {
         public static async Task Main(string[] args)
         {
+            SyncfusionLicenseProvider.RegisterLicense("NDMwNjQ2QDMxMzkyZTMxMmUzMG5yU0lkZHZSUGZoem5uMFQ5T0Q0bzNGYXBaS3l2Rm8zN2VXUGQrZWRDWTA9");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            
+            builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
     }
